776d1f7dc56bc06c23ba50a70e514e88f11bc603
Tiffany Forde <tiffanyforde98@gmail.com>
2026-02-04 15:25:28 -0400

    chore: log incoming requests

--- server-production.js
+++ server-production.js
@@ -44,6 +44,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log(`[REQ] ${req.method} ${req.url}`);
+  next();
+});
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
